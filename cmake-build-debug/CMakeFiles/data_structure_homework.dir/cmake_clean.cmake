8cced56868b6941e29e94d139c08fac602dcf5c0
ysj1173886760 <1173886760@qq.com>
2020-12-18 14:18:30 +0800

    second try

---
+++ cmake-build-debug/CMakeFiles/data_structure_homework.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure_homework.dir/main.cpp.o"
+  "data_structure_homework"
+  "data_structure_homework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structure_homework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
