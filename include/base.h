d4dda1b224f5df558c4ce4a2dc3df4f1c0552d4f
ysj1173886760 <1173886760@qq.com>
2020-12-24 08:49:53 +0800

    change

---
+++ include/base.h
@@ -0,0 +1,38 @@
+//
+// Created by sheep on 2020/12/24.
+//
+
+// base data container
+// set your own data container derived from this one
+
+#ifndef DATA_STRUCTURE_HOMEWORK_BASE_H
+#define DATA_STRUCTURE_HOMEWORK_BASE_H
+
+#include <string>
+#include <vector>
+
+class Base {
+public:
+    std::string id;      // all items should contain one unique ID
+};
+
+// here`s an example
+
+class ItemData: public Base {
+public:
+    std::string name;
+    double price;
+    std::string des;     // this is the description of a item
+
+};
+
+class UserData: public Base {
+public:
+    std::string real_name
+    std::string user_name
+    std::string pass_word
+    std::string phone_number
+    //etc
+};
+
+#endif //DATA_STRUCTURE_HOMEWORK_BASE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "test.h"
+#include "base.h"
 
 int main() {
     std::cout << TEST << std::endl;
