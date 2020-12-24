8f832aca46e33f5211f575a24ce760f6b5979978
ysj1173886760 <1173886760@qq.com>
2020-12-24 09:36:20 +0800

    change

--- include/base.h
+++ include/base.h
@@ -23,15 +23,19 @@ public:
     std::string name;
     double price;
     std::string des;     // this is the description of a item
+    int sell_num;
 
 };
 
 class UserData: public Base {
 public:
+    std::string account;
     std::string real_name;
     std::string user_name;
     std::string pass_word;
     std::string phone_number;
+    std::string email;
+    std::string id_number;
     //etc
 };
 
