cac59b468a67ba477a59d81773b163a940978bf7
dbdxzwh <2804272906@qq.com>
2020-12-26 14:29:04 +0800

    1

---
+++ include/QuestionSystem.h
@@ -0,0 +1,17 @@
+//
+// Created by dbdxzwh on 2020/12/26.
+//
+
+#ifndef DATA_STRUCTURE_HOMEWORK_QUESTIONSYSTEM_H
+#define DATA_STRUCTURE_HOMEWORK_QUESTIONSYSTEM_H
+
+#include "DB.h"
+#include "base.h"
+
+class QuestionSystem {
+public:
+
+};
+
+
+#endif //DATA_STRUCTURE_HOMEWORK_QUESTIONSYSTEM_H
