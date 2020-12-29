c12dee4b6038e3002451f94d077e19854c0bb7c9
ysj1173886760 <1173886760@qq.com>
2020-12-29 15:42:25 +0800

    ui update

--- include/UI.hpp
+++ include/UI.hpp
@@ -9,10 +9,12 @@ namespace UI {
     void mvwaddstring(WINDOW *win, int x, int y, const std::string& str) {
         mvwaddnstr(win, x, y, str.c_str(), str.size());
     }
+
     void mvwaddint(WINDOW *win, int x, int y, int value) {
         char *buffer = new char[80];
         sprintf(buffer, "%d", value);
         mvwaddstr(win, x, y, buffer);
         delete[] buffer;
     }
+
 }
--- test/ui_test.cpp
+++ test/ui_test.cpp
@@ -18,6 +18,7 @@ char *buffer;
 WINDOW *input;
 WINDOW *display;
 WINDOW *bar;
+WINDOW *alertWindow;
 
 int display_row;
 
@@ -84,6 +85,21 @@ void refresh_scr() {
     wrefresh(input);
 }
 
+void alert_win(WINDOW *win, const std::string& str) {
+    box(win, 0, 0);
+    noecho();
+    UI::mvwaddstring(win, 2, 3, str);
+    wattron(win, A_REVERSE);
+    mvwaddstr(win, 6, 18, "OK");
+    wattroff(win, A_REVERSE);
+    touchwin(win);
+    wrefresh(win);
+    getch();
+    echo();
+    touchwin(display);
+    wrefresh(display);
+}
+
 void init() {
     DB &db = DB::getInstance();
     db.open();
@@ -94,17 +110,18 @@ void init() {
 
     initscr();
     keypad(stdscr, TRUE);
-    halfdelay(10);
     input = newwin(3, 80, 21, 0);
     display = newwin(18, 80, 3, 0);
     bar = newwin(3, 80, 0, 0);
+    alertWindow = newwin(8, 40, 6, 20);
 
     curs_set(0);
 
-    bar_thread = std::thread(bar_stuff);
+    clear();
     clear_scr();
     draw();
     refresh_scr();
+    bar_thread = std::thread(bar_stuff);
 }
 
 void scanning_line(int y) {
@@ -115,16 +132,16 @@ void scanning_line(int y) {
 void init_animation() {
     char arr[24][80];
     memset(arr, 0, sizeof(arr));
-    strcpy(arr[7],  "             #    #    ######    #      #       ######                     ");
-    strcpy(arr[8],  "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[9],  "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[10], "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[11], "             ######    ######    #      #      #      #                    ");
-    strcpy(arr[12], "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[13], "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[14], "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[15], "             #    #    #         #      #      #      #                    ");
-    strcpy(arr[16], "             #    #    #######   ####   ####    ######                     ");
+    strcpy(arr[7],  "               #    #    ######    #      #       ######                     ");
+    strcpy(arr[8],  "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[9],  "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[10], "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[11], "               ######    ######    #      #      #      #                    ");
+    strcpy(arr[12], "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[13], "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[14], "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[15], "               #    #    #         #      #      #      #                    ");
+    strcpy(arr[16], "               #    #    #######   ####   ####    ######                     ");
     initscr();
     for (int i = 0; i < 80; i++) {
         scanning_line(i);
@@ -139,16 +156,39 @@ void init_animation() {
     endwin();
 }
 
+std::vector<std::string> prepare_command() {
+    std::string str = std::string(buffer);
+    vector<string> res;
+    int lst = 0;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == ',') {
+            res.push_back(str.substr(lst, i - lst));
+            lst = i + 1;
+        }
+    }
+    res.push_back(str.substr(lst, str.size() - lst));
+    return move(res);
+}
+
+void execute_command(const std::vector<std::string> commands) {
+    alert_win(alertWindow, "not implemented yet");
+}
+
 int main() {
     // make sure your terminal size is (24,80)
     init_animation();
     init();
+    std::vector<std::string> commands;
     while (1) {
         clear_scr();
         draw();
         get_input();
-        if (strcmp(buffer, "quit") == 0) break;
+        commands = prepare_command();
 
+        if (commands.empty()) continue;
+        if (commands.size() == 1 && commands[0] == "quit") break;
+
+        execute_command(commands);
         refresh_scr();
     }
 
